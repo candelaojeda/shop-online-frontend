5aa1054b57d8942d53d4f024c1985be16db86526
Candelaojeda <ojedacandelaco@gmail.com>
2021-07-28 12:18:34 -0300

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Catalogue from "./components/Catalogue";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-      <h1>HOLA!</h1>
+      <Router>
+        <div className="navigation">
+          <Link className="link-nav" to="/">
+            HOME
+          </Link>
+          <Link className="link-nav" to="/catalogue">
+            CATALOGUE
+          </Link>
+          <Link className="link-nav" to="/detail">
+            DETAILS
+          </Link>
+        </div>
+
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <div>
+                <h1>WELCOME TO THE PAGE !</h1>
+              </div>
+            );
+          }}
+        ></Route>
+        <Route path="/catalogue" component={Catalogue} />
+        <Route path="/detail" component={Detail} />
+      </Router>
     </div>
   );
 }
