0c11e164c45acd3fb4cc6198d66338e17f449ab8
Candelaojeda <ojedacandelaco@gmail.com>
2021-08-08 16:09:03 -0300

    added modal with bootstrap; yup and form validate

--- src/components/Formulary.js
+++ src/components/Formulary.js
@@ -2,45 +2,65 @@ import React from "react";
 import { Formik, Form } from "formik";
 import { TextField } from "./TextField";
 import * as Yup from "yup";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Formulary() {
+  const [smShow, setSmShow] = useState(false);
+
   const validate = Yup.object({
     userName: Yup.string()
-      .max(15, "Must be 15 characters or less.")
-      .required("UserName is required."),
+      .max(15, "Debe ingresar 15 caracteres o menos.")
+      .required("Nombre de usuario es requerido."),
     email: Yup.string()
-      .email("Email is invalid.")
-      .required("Email is required."),
+      .email("Email es invalido.")
+      .required("Email es requerido."),
     textarea: Yup.string()
-      .max(500, "Must be 500 characters or less.")
-      .min(10, "Must be 10 characters or more.")
-      .required("Text is required."),
+      .max(500, "Debe ingresar 500 caracteres o menos.")
+      .min(10, "Debe ingresar 10 caracteres o más.")
+      .required("Campo de texto es requerido."),
   });
 
   return (
-    <Formik
-      initialValues={{
-        userName: "",
-        email: "",
-        textarea: "",
-      }}
-      validationSchema={validate}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {(formik) => (
-        <div className="form-detail">
-          <Form>
-            <TextField label="Username" name="userName" type="text" />
-            <TextField label="Email" name="email" type="email" />
-            <TextField label="Query" name="textarea" type="textarea" />
-            <button className="btn-submit" type="submit">
-              SUBMIT A QUERY
-            </button>
-          </Form>
-        </div>
-      )}
-    </Formik>
+    <>
+      <h3>¿QUIERES DEJAR UNA CONSULTA? ¡EXPRÉSATE AQUÍ!</h3>
+      <Formik
+        initialValues={{
+          userName: "",
+          email: "",
+          textarea: "",
+        }}
+        validationSchema={validate}
+        onSubmit={() => {
+          setSmShow(true);
+        }}
+      >
+        {(formik) => (
+          <div className="form-detail">
+            <Form>
+              <TextField label="Usuario " name="userName" type="text" />
+              <TextField label="Email   " name="email" type="email" />
+              <TextField label="Consulta" name="textarea" type="textarea" />
+              <Button className="btn-submit" type="submit">
+                ENVIAR CONSULTA
+              </Button>
+              <Modal
+                size="sm"
+                show={smShow}
+                onHide={() => setSmShow(false)}
+                aria-labelledby="example-modal-sizes-title-sm"
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="example-modal-sizes-title-sm">
+                    ¡LISTO!
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Se ha enviado tu consulta con éxito.</Modal.Body>
+              </Modal>
+            </Form>
+          </div>
+        )}
+      </Formik>
+    </>
   );
 }
