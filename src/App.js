3798c826969dfc29ea155ce95e111ad7ebaccbe6
Candelaojeda <ojedacandelaco@gmail.com>
2021-08-09 17:52:15 -0300

    remove products shuffle in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Catalogue from "./components/Catalogue";
 import Detail from "./components/Detail";
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
 import Copyright from "./components/Copyright";
-import _ from "underscore";
+import Shuffle from "./components/Shuffle";
 
 function App() {
-  let [data, setData] = useState([]);
-  let url = "https://rooftop-api-rest-frontend.herokuapp.com/items?limit=4";
-
-  useEffect(
-    function () {
-      axios.get(url).then((res) => setData(res.data.items));
-    },
-    [url]
-  );
-
-  let shuffleArray = _.shuffle(data);
   return (
     <div className="App">
       <Router>
@@ -33,39 +20,7 @@ function App() {
               return (
                 <div>
                   <Carousel />
-                  <h3>MAS VISTOS...</h3>
-                  <div className="listProducts-home">
-                    {shuffleArray.map((shuffles) => (
-                      <div key={shuffles.id} className="products-home">
-                        <div className="title">{shuffles.title}</div>
-                        <div className="currency">
-                          {shuffles.currency}
-                          {shuffles.price}
-                        </div>
-                        <img
-                          src={shuffles.images[0]}
-                          alt="img"
-                          width="100%"
-                          height="60%"
-                        ></img>
-                      </div>
-                    ))}
-                  </div>
-                  <h4>TODOS LOS MEDIOS DE PAGO</h4>
-                  <div className="pay">
-                    <h5>
-                      <i class="fas fa-credit-card"></i>Tarjetas de crédito
-                    </h5>
-                    <h5>
-                      <i class="far fa-credit-card"></i>Tarjetas de débito
-                    </h5>
-                    <h5>
-                      <i class="fas fa-handshake"></i>Mercado pago
-                    </h5>
-                    <h5>
-                      <i class="fas fa-hand-holding-usd"></i>Efectivo
-                    </h5>
-                  </div>
+                  <Shuffle />
                 </div>
               );
             }}
